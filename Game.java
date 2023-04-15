2351beefcdc8a31dc9e31a3ed9c13d88a0e52fdd
terry-lws <terrylam299@gmail.com>
2023-04-15 23:56:20 +1000

    Add portal selection prompt

--- Game.java
+++ Game.java
@@ -1,21 +1,43 @@
-// import java.io.File;
-// import java.io.FileNotFoundException;
-// import java.util.Scanner;
+import java.util.Scanner;
 
 public class Game {
 
-    private static Portal.NorthPortal nPortal = new Portal.NorthPortal(null, 0, 0, 0);
-    private static Portal.SouthPortal sPortal = new Portal.SouthPortal(null, 0, 0, 0);
-    private static Portal.EastPortal ePortal = new Portal.EastPortal(null, 0, 0, 0);
-    private static Portal.WestPortal wPortal = new Portal.WestPortal(null, 0, 0, 0);
-
     public static void main(String[] args) {
         Portal.readExits("exits.txt");
-        //System.out.println(nPortal.getDirection());
         String playerName = Player.createName();
         Player player = new Player(playerName);
         playerName = player.getPlayerName();
         int coins = player.getCoins();
         System.out.println("Name: " + playerName + "\nCoins: " + coins);
+        String choice;
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("Select a portal (only enter a character)");
+            System.out.println("N - North");
+            System.out.println("E - East");
+            System.out.println("S - South");
+            System.out.println("W - West");
+            choice = scanner.nextLine().trim().toUpperCase();
+            switch (choice) {
+                case ("N"):
+                    System.out.println("Your selection: " + choice + " portal");
+                    Portal.nPortal.usePortal();
+                    break;
+                case ("E"):
+                    System.out.println("Your selection: " + choice + " portal");
+                    Portal.ePortal.usePortal();
+                    break;
+                case ("S"):
+                    System.out.println("Your selection: " + choice + " portal");
+                    Portal.sPortal.usePortal();
+                    break;
+                case ("W"):
+                    System.out.println("Your selection: " + choice + " portal");
+                    Portal.wPortal.usePortal();
+                    break;
+                default:
+                    System.out.println("No such portal, please choose again");
+            }
+        }
     }
 }
