5fdca9ce875faf3899de6dcfd4bd030cc20b5724
terry-lws <terrylam299@gmail.com>
2023-04-17 18:02:06 +1000

    Add printed messages for multiple occasions Add closed portal messages Add closed portal selection messages Add variable to detect new round

--- Game.java
+++ Game.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 
 public class Game {
 
+    public static boolean newRound = true;
+
     public static void main(String[] args) {
         Portal.readExits("exits.txt");
         String playerName = Player.createName();
@@ -12,28 +14,65 @@ public class Game {
         String choice;
         Scanner scanner = new Scanner(System.in);
         while (true) {
-            System.out.println("Select a portal (only enter a character)");
-            System.out.println("N - North");
-            System.out.println("E - East");
-            System.out.println("S - South");
-            System.out.println("W - West");
+            if (newRound) {
+                Portal.randomizeOpenPortal();
+                System.out.println("---------------------------------");
+            }
+            newRound = false;
+            System.out.println("Select a portal (enter one character only)");
+            if (Portal.nPortal.portalIsOpen)
+                System.out.println("N - North");
+            else
+                System.out.println("N - North (Closed)");
+            if (Portal.ePortal.portalIsOpen)
+                System.out.println("E - East");
+            else
+                System.out.println("E - East (Closed)");
+            if (Portal.sPortal.portalIsOpen)
+                System.out.println("S - South");
+            else
+                System.out.println("S - South (Closed)");
+            if (Portal.wPortal.portalIsOpen)
+                System.out.println("W - West");
+            else
+                System.out.println("W - West (Closed)");
             choice = scanner.nextLine().trim().toUpperCase();
             switch (choice) {
                 case ("N"):
-                    System.out.println("Your selection: " + choice + " portal");
-                    Portal.nPortal.usePortal();
+                    if (Portal.nPortal.portalIsOpen) {
+                        System.out.println("Your selection: " + choice + " portal");
+                        Portal.nPortal.usePortal();
+                        Portal.randomizePolicePresence(choice);
+                        newRound = true;
+                    } else
+                        System.out.println("This portal is closed!");
                     break;
                 case ("E"):
-                    System.out.println("Your selection: " + choice + " portal");
-                    Portal.ePortal.usePortal();
+                    if (Portal.ePortal.portalIsOpen) {
+                        System.out.println("Your selection: " + choice + " portal");
+                        Portal.ePortal.usePortal();
+                        Portal.randomizePolicePresence(choice);
+                        newRound = true;
+                    } else
+                        System.out.println("This portal is closed!");
                     break;
                 case ("S"):
-                    System.out.println("Your selection: " + choice + " portal");
-                    Portal.sPortal.usePortal();
+                    if (Portal.sPortal.portalIsOpen) {
+                        System.out.println("Your selection: " + choice + " portal");
+                        Portal.sPortal.usePortal();
+                        Portal.randomizePolicePresence(choice);
+                        newRound = true;
+                    } else
+                        System.out.println("This portal is closed!");
                     break;
                 case ("W"):
-                    System.out.println("Your selection: " + choice + " portal");
-                    Portal.wPortal.usePortal();
+                    if (Portal.wPortal.portalIsOpen) {
+                        System.out.println("Your selection: " + choice + " portal");
+                        Portal.wPortal.usePortal();
+                        Portal.randomizePolicePresence(choice);
+                        newRound = true;
+                    } else
+                        System.out.println("This portal is closed!");
                     break;
                 default:
                     System.out.println("No such portal, please choose again");
