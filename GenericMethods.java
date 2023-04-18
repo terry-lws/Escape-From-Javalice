e22d317f969a00b11fd9112791ccc03f2d6b92f0
terry-lws <terrylam299@gmail.com>
2023-04-18 23:45:13 +1000

    Create file Create class to store reuseable methods

---
+++ GenericMethods.java
@@ -0,0 +1,25 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.Random;
+
+public class GenericMethods {
+
+    //generate one double number
+    public static int randomizeOneToHundred() {
+        int randomNum = 0;
+        Random rand = new Random();
+        randomNum = rand.nextInt(100) + 1;
+        ;
+        return randomNum;
+    }
+
+    //sleep for predetermined miliseconds
+    public static void sleep(int time) {
+        try {
+            Thread.sleep(time);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
